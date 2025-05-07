aa312578971c64dbbadd7eaf690d25b89a357420
Mauricio <stb539@gmail.com>
2025-05-07 17:55:42 -0400

    ini

---
+++ capturador.py
@@ -0,0 +1,24 @@
+from selenium import webdriver  # pip install selenium
+from selenium.webdriver.chrome.options import Options
+import time
+
+# Inicializar el driver con opciones específicas
+options = Options()
+options.add_argument("--headless")  # Configurar Chrome en cabeza menos visible
+driver = webdriver.Chrome(options=options)  # Iniciar el driver de Chrome ***
+
+try:
+  # Ir al sitio web y capturar la pantalla ***
+    driver.get("https://www.google.com")
+
+    # Esperar hasta que la página se cargue completamente ***
+    time.sleep(3)
+
+    # Captura de pantalla del resultado ***
+    driver.save_screenshot("google.png")
+
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    driver.quit()  # Cerrar el driver ***
+  
